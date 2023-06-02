8600a6e60f84c1c5e3f7cef324a85f25b34108aa
Ankit <thatboyankit@gmail.com>
2023-06-02 21:09:20 +0530

    changed version to v0.8.15

--- foundry/src/Borrow.sol
+++ foundry/src/Borrow.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.15;
 
 import {ERC20} from "@oz/token/ERC20/ERC20.sol";
 import {Ownable} from "@oz/access/Ownable.sol";
